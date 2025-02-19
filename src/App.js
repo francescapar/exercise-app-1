83ae5b631f889893e17adc728090faecd1901120
francescapar <ftparodi29@gmail.com>
2025-02-19 12:10:12 -0500

    fixed github commit issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useCallback, useState } from 'react';
+import DurationExercise from './components/DurationExercise';
+import RepetitionExercise from './components/RepetitionExercise';
+
+const MENU_SCREEN = "menu";
+const EXERCISE_SCREEN = "exercise";
+const DURATION_EXERCISE = "duration";
+const REPETITION_EXERCISE = "repetition";
+
+let exerciseList = [
+  {type: DURATION_EXERCISE, name: "Running"},
+  {type: DURATION_EXERCISE, name: "Rowing"},
+  {type: DURATION_EXERCISE, name: "Swimming"},
+  {type: REPETITION_EXERCISE, name: "Push Ups"}
+];
 
 function App() {
+  let [currentScreen, setCurrentScreen] = useState(MENU_SCREEN);
+  let [currentExercise, setCurrentExercise] = useState("");
+  let screenComponent = undefined;
+  let buttonClick = useCallback((exercise) => {
+    setCurrentExercise(exercise);
+    setCurrentScreen(EXERCISE_SCREEN);
+  });
+
+  if (currentScreen === MENU_SCREEN) {
+    screenComponent = <div>
+      <p>Exercise Menu</p>
+      <ul>
+        {exerciseList.map((exercise) => {
+          return <li>
+            <button onClick={() => buttonClick(exercise)}>{exercise.name}</button>
+          </li>
+        })}
+      </ul>
+    </div>
+  } else if (currentScreen === EXERCISE_SCREEN) {
+    switch(currentExercise.type) {
+      case DURATION_EXERCISE:
+        screenComponent = <DurationExercise
+          exercise={currentExercise}
+          setMenuScreen={() => setCurrentScreen(MENU_SCREEN)}
+          />
+      break;
+      case REPETITION_EXERCISE:
+        screenComponent = <RepetitionExercise
+          exercise={currentExercise}
+          setMenuScreen={() => setCurrentScreen(MENU_SCREEN)}
+          />
+      break;
+      default:
+        screenComponent = undefined
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {screenComponent}
       </header>
     </div>
   );
